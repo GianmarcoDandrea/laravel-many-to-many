fa97b70e9574223bba329a7f38362d22e07d54fa
GianmarcoDandrea <gianm96@hotmail.it>
2024-01-29 22:31:05 +0100

    add: User Details

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Models\UserDetail;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+
     public function index()
     {
-        return view('admin.dashboard');
+        $user = Auth::user();
+        return view('admin.dashboard', compact('user'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,8 +4,41 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 mt-4">
-                <div>
-                    <h2>Hello {{ ucfirst(Auth::user()->name) }}! Welcome to your Project Portfolio</h2>
+                <div class="card text-bg-secondary mb-3">
+                    <div class="card-header">
+                        {{ __('Dashboard') }}
+                    </div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <h2 class="text-center">Hello {{ $user->name }}! Welcome to your personal project portfolio</h2>
+                        <h3 class="mt-5">Here's your details:</h3>
+
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item text-bg-secondary mb-3">
+                                <span class="fw-bold ">Email: </span>{{ $user->email }}
+                                
+                           </li>
+                
+                            <li class="list-group-item text-bg-secondary mb-3">
+                                <span class="fw-bold ">Address: 
+                                </span>{{ $user->userDetail ? $user->userDetail->address : 'No address available' }}
+                            </li>
+                
+                            <li class="list-group-item text-bg-secondary mb-3">
+                                <span class="fw-bold ">Email: </span> {{ $user->email }}
+                            </li>
+                 
+                            <li class="list-group-item text-bg-secondary mb-3 ">
+                                <span class="fw-bold">Phone Number: </span> {{ $user->userDetail ? $user->userDetail->phone : 'No phone available' }}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
